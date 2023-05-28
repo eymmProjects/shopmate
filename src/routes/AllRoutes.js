b590840e0a153059ca6f5c04710c1b47b6e43916
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-05-28 22:18:54 +0800

    updated all routes

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { Search, PageNotFound, ItemDetail, ItemList } from "../Pages";
+import { Search, PageNotFound, Home, Cart } from "../Pages";
 
 import React from "react";
 
@@ -7,12 +7,8 @@ export const AllRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path="" element={<ItemList />} />
-        <Route path="item/:id" element={<ItemDetail />} />
-        <Route path="items/women" element={<ItemList />} />
-        <Route path="items/men" element={<ItemList />} />
-        <Route path="items/shoes" element={<ItemList />} />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="search" element={<Search />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
