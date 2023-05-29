6255b6381b2aef324a3f893a059e37638767be4d
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-05-30 00:08:54 +0800

    Fix Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import { Link, NavLink } from "react-router-dom"; 
-import Logo from "../assets/logo.png"
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../assets/Logo.png";
 import "./Header.css";
 
 export const Header = () => {
@@ -10,12 +10,16 @@ export const Header = () => {
         <span>Shopping Cart</span>
       </Link>
       <nav className="navigation">
-        <NavLink to="/" className="link" end>Home</NavLink>
-        <NavLink to="/cart" className="link">Cart</NavLink>
+        <NavLink to="/" className="link" end>
+          Home
+        </NavLink>
+        <NavLink to="/cart" className="link">
+          Cart
+        </NavLink>
       </nav>
       <Link to="/cart" className="items">
         <span>Cart: 2</span>
       </Link>
     </header>
-  )
-}
+  );
+};
