0a8a9dd00ad8505b6a911663ffb772cbcd4c36db
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-05-30 23:46:31 +0800

    building

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -6,21 +6,6 @@ export const Cart = () => {
   const { total, cartList } = useCart();
   useTitle("Cart");
 
-  // const products = [
-  //   {
-  //     id: 1,
-  //     name: "Sony Wh-Ch510 Bluetooth Wireless",
-  //     price: 149,
-  //     image: "/assets/images/1001.png",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "boAt Rockerz 450",
-  //     price: 49,
-  //     image: "/assets/images/1002.png",
-  //   },
-  // ];
-
   return (
     <main>
       <section className="cart">
